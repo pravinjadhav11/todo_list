1cbe70ff2dcd976cbd260748e20b45d219bceba2
Pravin Jadhav <askpravinjadhav@gmail.com>
2024-01-11 11:43:50 +0530

    style

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
-let todoList = [
-    {
-      item: 'Buy Milk',
-      dueDate: '4/10/2023'
-    },
-    {
-      item: 'Go to College',
-      dueDate: '4/10/2023'
-    }
-  ];
+const inputBox = document.getElementById('input_box');
+const listContainer = document.getElementById('list-container');
+
+function addTask() {
+ if (inputBox.value === '') {
+  alert('Please enter a value');
   
-  displayItems();
-  
-  function addTodo() {
-    let inputElement = document.querySelector('#todo-input');
-    let dateElement = document.querySelector('#todo-date');
-    let todoItem = inputElement.value;
-    let todoDate = dateElement.value;
-    todoList.push({item: todoItem, dueDate: todoDate});
-    inputElement.value = '';
-    dateElement.value = '';
-    displayItems();
-  }
-  
-  function displayItems() {
-    let containerElement = document.querySelector('.todo-container');
-    let newHtml = '';
-    for (let i = 0; i < todoList.length; i++) {
-      let {item, dueDate} = todoList[i];
-      newHtml += `
-        <span>${item}</span>
-        <span>${dueDate}</span>
-        <button class='btn-delete' onclick="todoList.splice(${i}, 1);
-        displayItems();">Delete</button>
-      `;
-    }
-    containerElement.innerHTML = newHtml;
-  }
+ }else{
+  let li = document.createElement('li');
+  li.innerHTML = inputBox.value;
+  listContainer.appendChild(li);
+
+ }
+}
